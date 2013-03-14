c4a78fbffde1e3cc32c793667f4fc709f53e201c
Marcus Johansson <majohansson@gmail.com>
2013-03-14 18:31:08 +0100

    added missing property in Avatars

--- ScApi/Data/Avatars.cs
+++ ScApi/Data/Avatars.cs
@@ -2,6 +2,7 @@ namespace ScApi.Data
 {
     public class Avatars
     {
+        public bool IsSystemDefault { get; set; }
         public string Square16 { get; set; }
         public string Square30 { get; set; }
         public string Square45 { get; set; }
